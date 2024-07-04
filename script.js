61bab1b5210567541b54c5e8b0eb82091bc4129a
Sameerawan99 <165399018+Sameerawan99@users.noreply.github.com>
2024-07-04 15:01:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+// TASK # 01
+
+let str = " Hello World ";
+let trimmedstr = str.trim();
+console.log(trimmedstr);
+
+
+// task # 02
+
+let str = "Hello World";
+let substr = str.substring(5, 10);
+console.log(substr);
+
+// task # 03
+let str = "Hello World";
+let fivechar = str.slice(0, 5);
+console.log(fivechar);
+
+
+// task # 04
+let str = "Hello - World";
+let arr = str.split( '-' );
+console.log(arr);
+
+
+
+// Task # 05
+
+let arr =  ["hello", "world"] 
+let str = arr.join( ' ' );
+console.log(str);
+
+
+// Task # 06
+
+let str = "Hello World";
+let  index = str.indexOf('o');
+console.log(index);
+
+// Task # 07
+
+let str = "Hello World World";
+let Index = str.lastIndexOf('World');
+console.log(Index);
+
+// Task # 08
+
+let str = "World";
+let string = str.startsWith('Wor');
+console.log(string);
+
+// Task # 09
+
+let str = "Hello World";
+let endsWithWorld = str.endsWith('World');
+console.log(endsWithWorld); 
+
+
+// Task # 10
+ let str = "Hello";
+ let include = str.includes('o');
+ console.log(include);
